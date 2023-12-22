2b32da918485c1e56f7f5d9dc779c89e12ae669b
Syed Ashiq <imsyedashiq@gmail.com>
2023-12-22 18:28:30 +0600

    post delete api added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,8 +35,17 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/allTask', async (req, res) => {
-      const result = await taskCollection.find().toArray();
+    app.get('/allTask/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete('/allTask/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
       res.send(result);
     });
 
